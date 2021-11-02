b148c95f6491cb010e4f8c0ecb9637c575f43ac4
Existence.exe <pritishdutta20@gmail.com>
2021-11-02 09:22:07 +0530

    progress on the app

---
+++ app.js
@@ -0,0 +1,5 @@
+const button_1 = document.getElementById("button_1")
+console.log(button_1.innerText)
+function change(){
+    button_1.innerHTML = "Has been clicked";
+}
--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const electron = require('electron')
-const {app, BrowserWindow} = electron
- 
-app.on('ready', () => { 
+const { app, BrowserWindow } = electron
+
+app.on('ready', () => {
     console.log('Running...')
-     
-    const mainWindow = new BrowserWindow({ })
+
+    const mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        icon: __dirname + '/kakaotalk_icon_194149.ico',
+    })
     mainWindow.loadURL(`file://${__dirname}/index.html`)
 })
