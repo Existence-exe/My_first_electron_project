a1888d48b80a9928be407902e6a26a0fc44851a2
Existence.exe <pritishdutta20@gmail.com>
2021-12-13 23:22:26 +0530

    Welp form Bibu

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
-const name = document.getElementById("name_input").value;
-const pword = document.getElementById("pword_input").value;
-
+const names = document.getElementById("name_input");
+const pword = document.getElementById("pword_input");
+users = []
+ 
+function AddNewUser(){
+    let newUser = {
+        name: "",
+        pwd: ""
+    }
+    newUser.name = names.value
+    newUser.pwd = pword.value
+    users.push(newUser)
+    thisUser = users.find(object => object.name == names.value);
+    console.log("This user is", thisUser)
+    console.log("New User", newUser);
+    console.log("All users", users);
+  }
